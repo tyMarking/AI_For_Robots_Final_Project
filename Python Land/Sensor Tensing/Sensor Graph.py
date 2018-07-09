bdc5ecb01e54ce4c7c0f091e6de3485656159718
Ty Marking <30440916+tyMarking@users.noreply.github.com>
2018-07-08 23:10:11 -0700

    STarted sensor work

--- Python Land/Sensor Tensing/Sensor Graph.py
+++ Python Land/Sensor Tensing/Sensor Graph.py
@@ -1,3 +1,17 @@
+import matplotlib as plt
+import threading
+import time
+from HamsterAPI.comm_ble import *
+
+
+def graphit(robot):
+    while True:
+        prox = (robot.get_proximity(0),robot.get_proximity(1))
+        print("Prox L: " + str(prox[0]) + "\tProx R: " + str(prox[1]))
+        time.sleep(1)
+
+
+
 def main():
     max_robot_num = 1   # max number of robots to control
     comm = RobotComm(max_robot_num)
@@ -5,9 +19,9 @@ def main():
     print 'Bluetooth starts'
     robotList = comm.robotList
 
-    root = tk.Tk()
-    t_handle = BehaviorThreads(robotList)
-    gui = GUI(root, t_handle)
+    thread = threading.Thread(name="Graph Thread", target=graphit(robotList[0]))
+    thread.daemon = True
+    threat.start()
   
     root.mainloop()
 
@@ -16,3 +30,6 @@ def main():
 
 if __name__== "__main__":
   sys.exit(main())
+
+def __main__():
+    main()
