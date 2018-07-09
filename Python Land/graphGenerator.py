0f1ee9e240f57e167ead5f5e82c04c10c41b2ff2
admin <tymarking@gmail.com>
2018-07-09 13:12:50 -0700

    More work on maze

--- Python Land/graphGenerator.py
+++ Python Land/graphGenerator.py
@@ -1,6 +1,7 @@
-import Tkinter
+import Tkinter as tk
 import random
 import time
+from graph_with_edge_cost as Dijk
 def main():
 	#Num to generate
 	size = 4
@@ -14,21 +15,30 @@ def main():
 		if node < size**2-size and random.random() < 0.5:
 			graph[node].add(node+size)
 			graph[node+size].add(node)
-			
+
 	print graph
+	dijkGraph = Dijk.Graph()
+	for node in graph.keys():
+		dijkGraph.add_node(node,None)
+	for node in graph.keys():
+		for toNode in graph[node]:
+			dijkGraph.add_edge(node,toNode,1)
+
+
+	display(graph,size)
 
 
 def display(graph, size):
 	root = tk.Tk()
-
-	canvas = tk.Canvas(self.root, bg="white", width=400, height=400)
+	root.geometry="400x400"
+	canvas = tk.Canvas(root, bg="white", width=400, height=400)
 	canvas.pack()
 
 	pointXs = []
 	pointYs = []
 	dx = 400/(size+1)
 	dy = 400/(size+1)
-	for i in ragne(size):
+	for i in range(size):
 		pointXs.append((i+1)*dx)
 		pointYs.append((i+1)*dx)
 
@@ -41,13 +51,16 @@ def display(graph, size):
 			p2 = [x+dx,y+dy]
 			canvas.create_line(p1,p2,width=2)
 		if node + size in graph[node]:
+			print("HORIZONTAL LINE")
 			#draw line below
 			p1 = [x-dx,y+dy]
-			p2 = [x-dx,y-dy]
+			p2 = [x+dx,y+dy]
 			canvas.create_line(p1,p2,width=2)
 
 	root.mainloop()
-	time.sleep(10)
+
+
+	# time.sleep(10)
 
 
 if __name__ == "__main__":
